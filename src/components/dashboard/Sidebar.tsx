b9f838cb19a36ffcada28ad2bd19c483eaaf0c8a
Peter Paul Villarino <peterpaulvillarino@gmail.com>
2025-05-29 03:18:16 +0800

    feat: update for marketplace

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
 
 import { Button } from '../ui/button';
 import { SidebarNavItem } from './SidebarNavItem';
 import { HelpCenterCard } from './HelpCenterCard';
+import MarketPlacePopover from './AdmissionPopver';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 import { useUserStore } from '@/store/user';
@@ -26,6 +27,8 @@ export default function DashboardSidebar({ logout, children }: DashboardSidebarP
   const pathname = usePathname();
 
   const userStore = useUserStore(state => state.user);
+
+  const [isOpen, setIsOpen] = useState(false);
 
   if (userStore === null) return null;
 
@@ -155,43 +158,51 @@ export default function DashboardSidebar({ logout, children }: DashboardSidebarP
             }
           />
 
-          <SidebarNavItem
-            label="Marketplace"
-            isFooterItem={true}
-            href="/dashboard/marketplace"
-            icon={
-              <svg
-                width="16"
-                height="16"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M22 6V8.42C22 10 21 11 19.42 11H16V4.01C16 2.9 16.91 2 18.02 2C19.11 2.01 20.11 2.45 20.83 3.17C21.55 3.9 22 4.9 22 6Z"
-                />
-                <path
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2 7V21C2 21.83 2.94 22.3 3.6 21.8L5.31 20.52C5.71 20.22 6.27 20.26 6.63 20.62L8.29 22.29C8.68 22.68 9.32 22.68 9.71 22.29L11.39 20.61C11.74 20.26 12.3 20.22 12.69 20.52L14.4 21.8C15.06 22.29 16 21.82 16 21V4C16 2.9 16.9 2 18 2H7H6C3 2 2 3.79 2 6V7Z"
-                />
-                <path
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9 13.01H12M9 9.01H12M5.99 13H6.01M5.99 9H6.01"
-                />
-              </svg>
-            }
-          />
+          <div
+            className="relative"
+            onMouseEnter={() => setIsOpen(true)}
+          >
+            <SidebarNavItem
+              label="Marketplace"
+              isFooterItem={true}
+              href="/dashboard/marketplace"
+              icon={
+                <svg
+                  width="16"
+                  height="16"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M22 6V8.42C22 10 21 11 19.42 11H16V4.01C16 2.9 16.91 2 18.02 2C19.11 2.01 20.11 2.45 20.83 3.17C21.55 3.9 22 4.9 22 6Z"
+                  />
+                  <path
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2 7V21C2 21.83 2.94 22.3 3.6 21.8L5.31 20.52C5.71 20.22 6.27 20.26 6.63 20.62L8.29 22.29C8.68 22.68 9.32 22.68 9.71 22.29L11.39 20.61C11.74 20.26 12.3 20.22 12.69 20.52L14.4 21.8C15.06 22.29 16 21.82 16 21V4C16 2.9 16.9 2 18 2H7H6C3 2 2 3.79 2 6V7Z"
+                  />
+                  <path
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 13.01H12M9 9.01H12M5.99 13H6.01M5.99 9H6.01"
+                  />
+                </svg>
+              }
+            />
+
+            {isOpen && <MarketPlacePopover setIsOpen={setIsOpen} />}
+          </div>
+
           <SidebarNavItem
             label="Logout"
             isFooterItem={true}
