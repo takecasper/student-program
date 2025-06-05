09f0bb078dfc18e34f9599b893617f2e56d32240
zadpo <2htrading2022@gmail.com>
2025-06-05 16:42:52 +0800

    feat: refactor AddEvaluationForm component and enhance RoleUser layout
    - Replaced the existing content in AddEvaluationForm with a new FormBuilder component for improved form creation.
    - Removed unused state and UI elements to streamline the component.
    - Updated RoleUser component to ensure full-width layout for better responsiveness.

--- src/app/dashboard/(Program_Experience)/program/[id]/settings/components/RoleUser.tsx
+++ src/app/dashboard/(Program_Experience)/program/[id]/settings/components/RoleUser.tsx
@@ -114,11 +114,11 @@ const UserRole = () => {
   }, [partiallySelected]);
 
   return (
-    <div>
+    <div className="w-full">
       <h4 className="text-[12px] text-[#4f4f4f] font-medium mb-4">USER & ROLE</h4>
 
-      <div className={`grid grid-cols-1 lg:grid-cols-1 gap-6`}>
-        <Table>
+      <div className="w-full">
+        <Table className="w-full">
           <TableHeader>
             <TableRow className="border-b">
               <TableHead className="w-[50px] bg-[#fbfbfb] border-[#f5f5f5]">
